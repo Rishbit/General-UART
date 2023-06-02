10b53b5ef95efd4e48c97da9473c50c4dce5b3f0
Rishabh Tiwari <rishi13mar@gmail.com>
2023-06-03 01:21:31 +0530

    Update UART_Baud_Generator.v

--- srcs/UART_Baud_Generator.v
+++ srcs/UART_Baud_Generator.v
@@ -1,12 +1,15 @@
 `timescale 1ns / 1ps
 
-// Description          :       The BaudGenerator.v is used to divide the system clock by a factor necessary to obtain the required parameter. The module 
+// Description          :   1). General Description:
+//                              The BaudGenerator.v is used to divide the system clock by a factor necessary to obtain the required parameter. The module 
 //                          uses two parameters SYSTM_OPERN_FREQ and REQD_BAUD_RATE to calculate the division factor. Then the system clock is divided by 
 //                          the calculated factor and the output is provided to generate the baud clock.
-// Parameter Description:       The module receives two parameters viz., SYSTM_OPERN_FREQ, REQD_BAUD_RATE. These parameters are configured from the 
-//                          respective values provided by the UART Top Module and their functionalities.
-// Pin Description      :   1). systm_clock_in: This is the input clock signal on which the driver system works.
-//                          2). baud_clock_out: THis is the output clock signal needed to generate the required baudrate.
+//                          2). Parameter Description:
+//                              The module receives two parameters viz., SYSTM_OPERN_FREQ, REQD_BAUD_RATE. These parameters are configured from the respective 
+//                          values provided by the UART Top Module and their functionalities.
+//                          3). Pin Description:
+//                              1). systm_clock_in : This is the input clock signal on which the driver system works.
+//                              2). baud_clock_out : This is the output clock signal needed to generate the required baudrate.
 
 module UART_Baud_Generator #(parameter SYSTM_OPERN_FREQ = 11059200, parameter REQD_BAUD_RATE = 9600) (systm_clock_in, baud_clock_out);
     
@@ -32,6 +35,7 @@ module UART_Baud_Generator #(parameter SYSTM_OPERN_FREQ = 11059200, parameter RE
                             end
         endcase
     end
+    
     
     assign baud_clock_out = baud_clock_out_reg;
     
