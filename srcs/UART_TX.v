9e9fa5db1011958d63d2c5b5b0d1dc41cf849cc6
Rishabh Tiwari <rishi13mar@gmail.com>
2023-06-03 01:53:30 +0530

    Update UART_TX.v

--- srcs/UART_TX.v
+++ srcs/UART_TX.v
@@ -1,8 +1,37 @@
 `timescale 1ns / 1ps
 
-//discription:      The transmitter continues to transmit data if the data_valid signal is HIGH, even if the amount of data to transmit has excedded the value
-//              defined by the bytes_to_tx signal.
-
+// Description :    1). General Description:
+//                          This is the transmitter module for the N-Byte UART. It has the ability to transmit a specified number of bytes to an external register.
+//                      In order to transmit N bytes, the user specifies an input as (N-1) to a seperate line, before transmitting data. The module has the ability
+//                      to transmit any number of bytes ranging from 1 to 1024. Followed by the start bit, M.S.B. is transmitted first followed by other data bits 
+//                      uptill the L.S.B. and the stop bit.
+//                          It takes two clock cycles to transmit a single bit of data. It takes an aditional one clock cycle to wake up the UART from idle state.
+//                      The transmitter can be woken up from idle state by pulling the line LOW for one clock cycle. This is followed by a start bit spanning for 
+//                      two clock cycles, followed by 8 data bits and the stop bit, marked by pulling the line HIGH for two clock cycles. If there are more bytes 
+//                      to be transmitted, a start bit is sent again and the process continues until all the bytes are transferred. After all the bytes are 
+//                      transferred, the transmitter goes back to idle state.
+//                  2). State Descriptions:
+//                      REC_STATE_IDLE: The receiver is idle in this state, which means that no activities are being performed, and the receiver is awaiting a
+//                                      data to receive. In order to begin transmission, the receiver line is first pulled LOW for one clock cycle. This puts
+//                                      the UART in REC_STATE_STRT state, and the transmission begins.
+//                      REC_STATE_STRT: This is the state where the reciver performs the reception of the start bit. If the serial data input data line is LOW for
+//                                      two clock cycles after completing the IDLE state transition period (one clock cycle), the start bit is received, the 
+//                                      transmission is confirmed and the device goes into the data reception state. If the data line is pulled HIGH again, it 
+//                                      means that there is no data to receive and the device goes back to the idle state REC_STATE_IDLE.
+//                      REC_STATE_DATA: This is the data reception state. Two clock cycles are required to receive every bit. After all 8 bits are received, the 
+//                                      receiver goes into the REC_STATE_STOP, where it receives the stop bit.
+//                      REC_STATE_STOP: The receiver receives the stop bit in this state. It requires the serial data line to stay HIGH for two clock cycles to 
+//                                      receive the stop bit. After completing the reception of stop bit, the receiverr decides which state tomove to. If there are
+//                                      more bytes to transfer, the receiver goes to REC_STATE_STRT, where it has to receive the start bit for next transmission.
+//                                      If there are no more bytes to transfer, the receiver goes back to REC_STATE_IDLE state.
+//                  3). Pin Descriptions:
+//                        clock          : Input clock signal obtained from the UART_Baud_Generator module that operates the receiver with the specified baud rate.
+//                        bytes_to_rx    : This is a 10-bit input array, used to configure the amount of bytes of data that has to be received from the UART 
+//                                         module. This is configured in real time by the driving device.
+//                        serial_data_in : This is the serial data input to the receiver.
+//                        rx_data_valid  : This output signal indicates whether the received byte of data is valid for processing by the driving device. Any 
+//                                         number of new bytes.
+//                        rx_data_byte   : This is the byte of data received by the driving device.
 
 module UART_TX (clock, bytes_to_tx, tx_data_byte, tx_data_valid, /*tx_clk_ctr, tx_state, tx_bit_ctr, bytes_to_tx_reg,*/ serial_data_out);
     
